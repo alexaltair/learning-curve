efbb231f7f9008ff503e74be96419504ebf83977
Dave Ferris <davidmferris@gmail.com>
2013-08-02 16:37:59 -0700

    add YoutubeIt to gem file

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,7 @@
+class AnswersController < ApplicationController
+
+  def index
+
+  end
+
+end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -14,6 +14,7 @@
 class Answer < ActiveRecord::Base
   attr_accessible :caption, :question_id, :user_id, :video
 
-  belongs_to :question, :user
+  belongs_to :question
+  belongs_to :user
   has_many :votes, as: :votable
 end
