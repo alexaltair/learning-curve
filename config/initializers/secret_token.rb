45d9b31101f3ad9d06ecd419faffeaf11e1e2eee
Alex Altair <alexanderaltair@gmail.com>
2013-08-02 10:39:18 -0700

    Starts the empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningCurve::Application.config.secret_token = '066933649db4d488cf5b3b4cbcece165252509d2d38b2820d531b62adf3307ddcf94643fe025a95affc3d6c69060f835d122a1be78241033fca3e8225522fced'
