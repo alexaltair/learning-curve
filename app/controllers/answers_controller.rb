4f297a2b9f823d1deee46ec30f559ac3371dbfde
Dave Ferris <davidmferris@gmail.com>
2013-08-03 09:59:15 -0700

    make routes and forms

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,26 @@
 class AnswersController < ApplicationController
 
-  def index
 
+
+  def index
+    @answers = Answer.all
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def upload
+    @answer = Answer.create(params[:answer])
+    if @answer
+      @upload_info = Answer.token_form(params[:answer], answers_path)
+    end
+  end
+
+  def save_video
+  end
+
+  def show
   end
 
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -12,9 +12,31 @@
 #
 
 class Answer < ActiveRecord::Base
+  require "youtube_it"
+
+
   attr_accessible :caption, :question_id, :user_id, :video
 
   belongs_to :question
   belongs_to :user
   has_many :votes, as: :votable
+
+  def self.yt_session
+     @yt_session ||= YouTubeIt::Client.new(:username => "learningcurvelabs@gmail.com" , :password => "password" , :dev_key => "dev_key")
+  end
+
+  def self.token_form(params, nexturl)
+    yt_session.upload_token(video_options(params), nexturl)
+  end
+
+  def self.video_options(params)
+    opts = {:title => "title",
+     :caption => params[:caption],
+     :category => "People",
+     :keywords => ["test"]}
+    end
+
+
+
+
 end
