05e43104460fab8b7e7668b9bc92ca7cfee4a79f
Xiao G. Wu <xiaogwu@gmail.com>
2013-08-06 13:39:19 -0700

    Add Admin role to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806004528) do
+ActiveRecord::Schema.define(:version => 20130806201156) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(:version => 20130806004528) do
     t.string   "name"
     t.string   "password"
     t.string   "password_confirmation"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130806004528) do
     t.string   "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
